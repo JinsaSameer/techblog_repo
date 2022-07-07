b6151dc6e3ffe4091f505be60c0cc3f05c846819
JinsaSameer <jinsajaleel@gmail.com>
2022-07-06 22:51:18 -0700

    updated files

---
+++ Develop/controllers/homeRoutes.js
@@ -0,0 +1,55 @@
+const {User, Post} = require("../models");
+
+const router = require("express").Router();
+
+router.get("/", async (req, res) => {
+
+    try{
+        const homepageData = await Post.findAll(
+            {
+                attributes: ["id", "title", "createdAt"],
+                include: {
+                    model: User,
+                    attributes: ["username"]
+                }
+            }
+        ).catch((err) => {
+            res.json(err);
+        })
+        
+        const posts = homepageData.map(post => post.get({plain: true}));
+        
+        //render with handlebars
+        res.render("homepage", {
+            posts, 
+            logged_in: req.session.logged_in
+        })
+
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
+
+router.get("/single/:id", async (req,res) => {
+    try {
+        if(!req.session.logged_in){
+            res.redirect("/login");
+            return;
+        }
+        const postDataDb = await Post.findByPk(req.params.id,{
+            attributes: ["id", "title","content", "createdAt"],
+            include: {
+                model: User,
+                attributes: ["username"]
+            },
+        })
+        
+       
+        
+        
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
